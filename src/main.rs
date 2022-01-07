e728a65ef063f99946f08ce92886cd8675f2e64b
GO <goxayyasang@gmail.com>
2022-01-07 13:41:52 +0700

    palindrome

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use std::io;
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    if is_palindrome(input.trim().to_string()) {
+        println!("{} is palindrome",input);
+    }else {
+        println!("{} is not palindrome",input);
+    } 
+}
+
+fn is_palindrome(palindrome: String) -> bool {
+    let palindrome_length = palindrome.len();
+    let is_even = palindrome_length % 2 == 0;
+    if is_even {
+        let front_palindrom = &palindrome[..palindrome_length / 2];
+        let back_palindrom = &palindrome[(palindrome_length / 2)..]
+            .chars()
+            .rev()
+            .collect::<String>();
+        if front_palindrom == back_palindrom {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        let front_palindrom = &palindrome[..palindrome_length / 2];
+        let back_palindrom = &palindrome[(palindrome_length / 2) + 1..]
+            .chars()
+            .rev()
+            .collect::<String>();
+        if front_palindrom == back_palindrom {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
